a94632c6fdff66466fec0849acc2b7dcf336c0e4
SHASHWAT  GARG <gargshashwat705@gmail.com>
2023-08-20 14:51:58 +0530

    complete code

---
+++ frontend/src/hoc/withAdminAuth.js
@@ -0,0 +1,18 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+export const withAdminAuth = (auth) => (WrappedComponent) => {
+  return (props) => {
+    const { replace } = useHistory();
+    const admin = useSelector((state) => state.admin.admin);
+
+    useEffect(() => {
+      if (auth && !admin) replace("/admin/login");
+      if (!auth && admin) replace("/admin");
+    }, [admin]);
+
+    return <WrappedComponent {...props} />;
+  };
+};
---
+++ frontend/src/utils.js
@@ -0,0 +1,11 @@
+/* eslint-disable eqeqeq */
+let history;
+
+const setHistory = (historyObj) => (history = historyObj);
+
+const filterProducts = (products, filter) => {
+  if (filter == "All") return products;
+  return products.filter((product) => product.category.name == filter);
+};
+
+export { history, setHistory, filterProducts };
